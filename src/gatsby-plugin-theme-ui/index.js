855d43a0c6bf562e4ef79c9c62013960c1fb44dd
eunsonny <eunsonnny@gmail.com>
2023-04-23 21:20:14 +0900

    Modify: React-Query 글 다듬기

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -72,17 +72,17 @@ const theme = merge(tailwind, {
     },
     h1: {
       variant: `text.heading`,
-      fontSize: [4, 5, 5, 6],
+      fontSize: [3, 4, 4, 5],
       mt: 4,
     },
     h2: {
       variant: `text.heading`,
-      fontSize: [3, 4, 4, 5],
+      fontSize: [2, 3, 3, 4],
       mt: 4,
     },
     h3: {
       variant: `text.heading`,
-      fontSize: [2, 3, 3, 4],
+      fontSize: [1, 2, 2, 3],
       mt: 4,
     },
     h4: {
